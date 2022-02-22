35400153243729f5bcb8b9545ac123c8628e1ab0
James <mburukungu@gmail.com>
2022-02-22 09:46:13 +0300

    installed apps edited

--- cboard/settings.py
+++ cboard/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'meetings.apps.MeetingsConfig'
+    'meetings.apps.MeetingsConfig',
 ]
 
 MIDDLEWARE = [
