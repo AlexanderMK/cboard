067ef691d7fd1122dae0769e8baa0e49d967f231
James <mburukungu@gmail.com>
2022-02-22 12:45:47 +0300

    main path edited

--- cboard/urls.py
+++ cboard/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('meetings/', include('meetings.urls')),
+    path('cboard/', include('meetings.urls')),
    
 ]
